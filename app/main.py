8814256b23e393edb0062d4b13bb9b6624c05ce4
jcribby <joelcribby@live.ca>
2018-03-03 17:40:28 -0400

    Update

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-import os
 import bottle
+import os
 import random
+
+
 
 @bottle.route('/')
 def static():
@@ -11,14 +13,15 @@ def static():
 def static(path):
     return bottle.static_file(path, root='static/')
 
-@bottle.route
+
+@bottle.post('/start')
 def start():
     data = bottle.request.json
     game_id = data.get('game_id')
     board_width = data.get('width')
     board_height = data.get('height')
 
-    head_url = '%s://%s/static/head.png' % (
+    head_url = 'SeverusSnape.jpg' % (
         bottle.request.urlparts.scheme,
         bottle.request.urlparts.netloc
     )
@@ -31,31 +34,28 @@ def start():
         'head_url': head_url
     }
 
+
 @bottle.post('/move')
 def move():
     data = bottle.request.json
 
     # TODO: Do things with data
-
+    
     directions = ['up', 'down', 'left', 'right']
     direction = random.choice(directions)
     print direction
     return {
         'move': direction,
-        'taunt': 'battlesnake-python!'
+        'taunt': 'Turn to page 394'
     }
 
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 
-
-
-
 if __name__ == '__main__':
     bottle.run(
-	application,
-	host=os.getenv('IP', '0.0.0.0'),
-	port=os.getenv('PORT', '8080'),
-	debug = True)
-    
+        application,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=os.getenv('PORT', '8080'),
+        debug = True)
